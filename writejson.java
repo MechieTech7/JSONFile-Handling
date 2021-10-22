09c8617ef3016bf27b67f902614e96b2d75e1cc8
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-10-22 14:15:31 +0530

    Create writejson.java

---
+++ writejson.java
@@ -0,0 +1,46 @@
+package json;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteJSONFile
+{
+    @SuppressWarnings("unchecked")
+    public static void main( String[] args )
+    {
+        //First Employee
+        JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Murthy");
+
+
+        JSONObject employeeObject = new JSONObject();
+        employeeObject.put("employee", employeeDetails);
+
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "MS");
+        employeeDetails2.put("lastName", "Dhoni");
+
+        JSONObject employeeObject2 = new JSONObject();
+        employeeObject2.put("employee", employeeDetails2);
+
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+
+        //Write JSON file
+        try (FileWriter file = new FileWriter("employees.json")) {
+
+            file.write(employeeList.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
